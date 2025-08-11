6c0625eaa1b9800364eb14fbb949e739c3d4847e
Spunch <137802895+Spunchkin@users.noreply.github.com>
2025-08-11 19:55:43 +0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Compile the program
-gcc -Wall -Wextra -pedantic -std=c99 src/bin.c src/file_operations.c src/text_editor_functions.c -lncurses -o ben
-
-# Move the executable to a directory in PATH
-sudo mv ben /usr/local/bin
-
-echo "Setup complete. You can now run 'ben' from anywhere in your terminal."
