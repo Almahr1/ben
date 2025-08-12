704741a59c5177408018487490ea1306c15d5731
Spunch <137802895+Spunchkin@users.noreply.github.com>
2025-08-12 08:20:58 +0400

    Create color_config.h

---
+++ src/color_config.h
@@ -0,0 +1,31 @@
+#ifndef COLOR_CONFIG_H
+#define COLOR_CONFIG_H
+
+#include <ncurses.h>
+
+// Color Configuration - Change these values to customize the editor appearance
+// Available colors: COLOR_BLACK, COLOR_RED, COLOR_GREEN, COLOR_YELLOW, 
+//                  COLOR_BLUE, COLOR_MAGENTA, COLOR_CYAN, COLOR_WHITE
+
+#define TEXT_FG_COLOR           COLOR_CYAN
+#define TEXT_BG_COLOR           COLOR_BLACK
+#define LINE_NUMBER_FG_COLOR    COLOR_BLUE
+#define LINE_NUMBER_BG_COLOR    COLOR_BLACK
+#define CURSOR_LINE_FG_COLOR    COLOR_WHITE
+#define CURSOR_LINE_BG_COLOR    COLOR_BLACK
+#define STATUS_BAR_FG_COLOR     COLOR_WHITE
+#define STATUS_BAR_BG_COLOR     COLOR_BLUE
+#define COMMAND_INPUT_FG_COLOR  COLOR_CYAN
+#define COMMAND_INPUT_BG_COLOR  COLOR_BLACK
+
+// Color Pair Definitions (don't change these numbers, they're used internally)
+#define COLOR_PAIR_TEXT         1
+#define COLOR_PAIR_LINE_NUMBERS 2
+#define COLOR_PAIR_STATUS_BAR   3
+#define COLOR_PAIR_COMMAND      4
+#define COLOR_PAIR_CURSOR_LINE  5
+
+// Function to initialize all color pairs
+void init_editor_colors(void);
+
+#endif
