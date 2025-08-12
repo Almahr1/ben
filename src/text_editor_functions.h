fe5875f9754b4d35feb8e1080ecbf72397c66399
Omar Almahri <omar-almahri@spunchkin.com>
2025-08-12 14:55:42 +0400

    Reapply "Added Normal Mode, Added Indicator for current mode"
    This reverts commit b0ea21955fbe14cc1d42dfb56600ac8a1ebff5ad.

--- src/text_editor_functions.h
+++ src/text_editor_functions.h
@@ -7,8 +7,15 @@
 
 #define MAX_COMMAND_LENGTH 30
 
+// Editor mode definitions
+typedef enum {
+    MODE_NORMAL,
+    MODE_INSERT,
+    MODE_COMMAND
+} EditorMode;
+
 extern int top_line;
-extern int special_mode;
+extern EditorMode current_mode;
 
 // File operations
 void saveToFile(const char *filename, TextBuffer *buffer);
@@ -18,13 +25,16 @@ void loadFromFile(const char *filename, TextBuffer *buffer);
 void drawLineNumbers(int visible_lines, const TextBuffer *buffer);
 void drawTextContent(int visible_lines, const TextBuffer *buffer);
 void drawStatusBar(const char *filename, const TextBuffer *buffer, const char *command);
+void drawModeIndicator(EditorMode mode);
 
 // Input handling
 void handleNormalModeInput(int ch, TextBuffer *buffer);
-void handleSpecialModeInput(int ch, char *command, TextBuffer *buffer, const char *filename);
+void handleInsertModeInput(int ch, TextBuffer *buffer);
+void handleCommandModeInput(int ch, char *command, TextBuffer *buffer, const char *filename);
 void handleInput(char *command, TextBuffer *buffer, const char *filename);
 
 // Utility functions
 int get_absolute_line_number(TextBuffer *buffer, Line *target_line);
+const char* get_mode_string(EditorMode mode);
 
 #endif
